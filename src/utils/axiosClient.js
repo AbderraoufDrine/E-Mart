36d6c3a51ef04ebc5ef7302de6ce12a09c9355c1
Abderraouf <abderraoufdrine@gmail.com>
2024-04-14 00:54:51 -0400

    Modified URL

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const key = process.env.NEXT_PUBLIC_REST_API_KEY;
-const Url = "http://localhost:1337/api";
+const Url = process.env.NEXT_PUBLIC_URL;
 
 const client = axios.create({
   baseURL: Url,
